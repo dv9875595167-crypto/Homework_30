fb0b010f16b9c46e44a318f482c3772261c893b2
markelovadacha91@mail.ru <markelovadacha91@mail.ru>
2026-02-28 14:57:35 +0300

    Fix import order and formatting for factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,34 +1,34 @@
 import random
-
 import factory
+from faker import Faker as FakerLib
 
 from hw.app import db
 from hw.models import Client, Parking
 
+fake = FakerLib(locale="en_US")
 
 class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Client
         sqlalchemy_session = db.session
 
-    name = factory.Faker("first_name", locale="en_US")
-    surname = factory.Faker("last_name", locale="en_US")
+    name = factory.LazyFunction(fake.first_name)
+    surname = factory.LazyFunction(fake.last_name)
     credit_card = factory.Maybe(
-        factory.Faker("pybool", locale="en_US"),
-        yes_declaration=factory.Faker("credit_card_number", locale="en_US"),
+        factory.LazyFunction(lambda: fake.boolean()),
+        yes_declaration=factory.LazyFunction(fake.credit_card_number),
         no_declaration=None,
     )
-    car_number = factory.Faker("text", max_nb_chars=50, locale="en_US")
-
+    car_number = factory.LazyFunction(lambda: fake.text(max_nb_chars=50))
 
 class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Parking
         sqlalchemy_session = db.session
 
-    address = factory.Faker("address", locale="en_US")
-    opened = factory.Faker("pybool", locale="en_US")
-    count_places = factory.Faker("pyint", min_value=1, max_value=50, locale="en_US")
+    address = factory.LazyFunction(fake.address)
+    opened = factory.LazyFunction(lambda: fake.boolean())
+    count_places = factory.LazyFunction(lambda: fake.random_int(min=1, max=50))
     count_available_places = factory.LazyAttribute(
         lambda obj: random.randint(0, obj.count_places)
     )
