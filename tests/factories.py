e13f118a9eaa20d318bc0344d10bdab809b6ff61
markelovadacha91@mail.ru <markelovadacha91@mail.ru>
2026-02-28 14:33:06 +0300

    Update project structure: remove Pylint, move requirements, update tests

--- tests/factories.py
+++ tests/factories.py
@@ -31,3 +31,4 @@ class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
     count_available_places = factory.LazyAttribute(
         lambda obj: random.randint(0, obj.count_places)
     )
+    
