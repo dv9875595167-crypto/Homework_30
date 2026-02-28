2145a80cb7b94e97bfc023fb5003f762273e1274
markelovadacha91@mail.ru <markelovadacha91@mail.ru>
2026-02-28 14:27:10 +0300

    Update project structure: remove Pylint, move requirements, update tests

--- tests/test_with_factory.py
+++ tests/test_with_factory.py
@@ -1,5 +1,5 @@
 from hw.models import Client, Parking
-from hw.tests.factories import ClientFactory, ParkingFactory
+from .factories import ClientFactory, ParkingFactory
 
 
 def test_create_client(app, db):
