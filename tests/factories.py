02bb3036e52bf7e83bf24fb1e06c297d55a52026
markelovadacha91@mail.ru <markelovadacha91@mail.ru>
2026-02-28 14:50:17 +0300

    Fix import order and formatting for factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -14,11 +14,11 @@ class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
     name = factory.Faker("first_name", locale="en_US")
     surname = factory.Faker("last_name", locale="en_US")
     credit_card = factory.Maybe(
-        factory.Faker("pybool"),
+        factory.Faker("pybool", locale="en_US"),
         yes_declaration=factory.Faker("credit_card_number", locale="en_US"),
         no_declaration=None,
     )
-    car_number = factory.Faker("text", max_nb_chars=50)
+    car_number = factory.Faker("text", max_nb_chars=50, locale="en_US")
 
 
 class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -27,8 +27,8 @@ class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = db.session
 
     address = factory.Faker("address", locale="en_US")
-    opened = factory.Faker("pybool")
-    count_places = factory.Faker("pyint", min_value=1, max_value=50)
+    opened = factory.Faker("pybool", locale="en_US")
+    count_places = factory.Faker("pyint", min_value=1, max_value=50, locale="en_US")
     count_available_places = factory.LazyAttribute(
         lambda obj: random.randint(0, obj.count_places)
     )
